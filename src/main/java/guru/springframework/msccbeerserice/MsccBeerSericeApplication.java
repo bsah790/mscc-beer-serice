95f61614d1b6a0f0a03ee05cb222e3221e8825e7
bsah790 <baleshwar.cse@gmail.com>
2020-04-19 09:08:32 +0530

    Initial project add. Closes #1

---
+++ src/main/java/guru/springframework/msccbeerserice/MsccBeerSericeApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msccbeerserice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBeerSericeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsccBeerSericeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/msccbeerserice/MsccBeerSericeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msccbeerserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBeerSericeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
